7aad1580478736e40e72011ebaa637eab94c3b90
Nicolas Carvalho <nicolaspessoal@icloud.com>
2021-01-14 16:15:04 -0300

    Add Infra layer

---
+++ InfraTests/AlamofireAdapterTests.swift
@@ -0,0 +1,14 @@
+//
+//  InfraTests.swift
+//  InfraTests
+//
+//  Created by Nicolas Carvalho on 14/01/21.
+//
+
+import XCTest
+
+class AlamofireAdapterTests: XCTestCase {
+    func test_() {
+        
+    }
+}
