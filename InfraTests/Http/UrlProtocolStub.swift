24a14faed23f43f4542369209f871bd167b1db87
Nicolas Carvalho <ncarvalho.modalgr@ext.valid.com>
2021-01-15 12:13:44 -0300

    Move UrlProtocolStub to its own file

---
+++ InfraTests/Http/UrlProtocolStub.swift
@@ -0,0 +1,49 @@
+//
+//  UrlProtocolStub.swift
+//  InfraTests
+//
+//  Created by Nicolas Carvalho on 15/01/21.
+//
+
+import Foundation
+
+class UrlProtocolStub: URLProtocol {
+    static var emit: ((URLRequest) -> Void)?
+    static var data: Data?
+    static var response: HTTPURLResponse?
+    static var error: Error?
+    
+    static func observeRequest(completion: @escaping (URLRequest) -> Void) {
+        UrlProtocolStub.emit = completion
+    }
+    
+    static func simulate(data: Data?, response: HTTPURLResponse?, error: Error?) {
+        UrlProtocolStub.data = data
+        UrlProtocolStub.response = response
+        UrlProtocolStub.error = error
+    }
+    
+    open override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+    
+    open override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    open override func startLoading() {
+        UrlProtocolStub.emit?(request)
+        if let data = UrlProtocolStub.data {
+            client?.urlProtocol(self, didLoad: data)
+        }
+        if let response = UrlProtocolStub.response {
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+        }
+        if let error = UrlProtocolStub.error {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+        client?.urlProtocolDidFinishLoading(self)
+    }
+    
+    open override func stopLoading() {}
+}
