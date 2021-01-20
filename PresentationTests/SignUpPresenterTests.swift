7b70bc9a127395cf8d42d6408205ef942966f102
Nicolas Carvalho <nicolaspessoal@icloud.com>
2021-01-20 11:41:49 -0300

    Move SystemUnderTest creation to a factory method

--- PresentationTests/SignUpPresenterTests.swift
+++ PresentationTests/SignUpPresenterTests.swift
@@ -17,7 +17,7 @@ class SignUpPresenter {
     func signUp(viewModel: SignUpViewModel) {
         if viewModel.name == nil || viewModel.name!.isEmpty {
             alertView.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: "O campo Nome é obrigatório"))
-        }	
+        }
     }
 }
 
@@ -39,8 +39,7 @@ struct SignUpViewModel {
 
 class SignUpPresenterTests: XCTestCase {
     func test_signUp_should_show_error_message_if_name_is_not_provided() {
-        let alertViewSpy = AlertViewSpy()
-        let systemUnderTest = SignUpPresenter(alertView: alertViewSpy)
+        let (systemUnderTest, alertViewSpy) = makeSystemUnderTest()
         let signUpViewModel = SignUpViewModel(email: "any_email@mail.com", password: "any_password", passwordConfirmation: "any_password")
         systemUnderTest.signUp(viewModel: signUpViewModel)
         XCTAssertEqual(alertViewSpy.viewModel, AlertViewModel(title: "Falha na validação", message: "O campo Nome é obrigatório"))
@@ -48,6 +47,12 @@ class SignUpPresenterTests: XCTestCase {
 }
 
 extension SignUpPresenterTests {
+    func makeSystemUnderTest() -> (systemUnderTest: SignUpPresenter, alertViewSpy: AlertViewSpy) {
+        let alertViewSpy = AlertViewSpy()
+        let systemUnderTest = SignUpPresenter(alertView: alertViewSpy)
+        return (systemUnderTest, alertViewSpy)
+    }
+    
     class AlertViewSpy: AlertView {
         var viewModel: AlertViewModel?
         
